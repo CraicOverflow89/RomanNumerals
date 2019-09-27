8d4bcdd789370ae5bfa36b60a9d7153bd8fd3c0f
CraicOverflow89 <craic.overflow@hotmail.com>
2019-09-27 08:50:25 +0100

    Noted issues.

--- src/main/kotlin/craicoverflow89/romannumerals/test.kt
+++ src/main/kotlin/craicoverflow89/romannumerals/test.kt
@@ -3,9 +3,10 @@ package craicoverflow89.romannumerals
 fun main(args: Array<String>) {
 
     // Test: Int to Numerals
-    val inputInt = 3675
+    val inputInt = 1984
     println("int = $inputInt")
     println(RomanNumerals.parseInt(inputInt))
+    // NOTE: missing logic for when 19xx should be MCM (currently results in MDCCCC)
 
     // Test: Numerals to Int
     //val inputNumerals = "MMMCDXXLV"
