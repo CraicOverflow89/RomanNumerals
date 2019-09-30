128f4ece8108a8ef3069f9997957b6a5393e5055
CraicOverflow89 <craic.overflow@hotmail.com>
2019-09-30 08:27:23 +0100

    Created test with many assertions for the parseInt method.

---
+++ src/test/kotlin/craicoverflow89/romannumerals/RomanNumeralsTest.kt
@@ -0,0 +1,31 @@
+package craicoverflow89.romannumerals
+
+import org.hamcrest.CoreMatchers
+import org.junit.Assert
+import org.junit.Test
+
+class RomanNumeralsTest {
+
+    @Test
+    fun parseInt() = with(fun(input: Int, expected: String) {
+        Assert.assertEquals(expected, RomanNumerals.parseInt(input))
+    }) {
+        this(0, "")
+        this(7, "VII")
+        this(9, "IX")
+        this(10, "X")
+        this(13, "XIII")
+        this(22, "XXII")
+        this(29, "XXIX")
+        this(41, "XLI")
+        this(58, "LVIII")
+        this(69, "LXIX")
+        this(84, "LXXXIV")
+        this(117, "CXVII")
+        this(496, "CDXCVI")
+        this(859, "DCCCLIX")
+        this(1301, "MCCCI")
+        this(1989, "MCMLXXXIX")
+    }
+
+}
